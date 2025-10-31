f3a3397de4fcc598ae7bd4ab412074e893526774
Shoukoraa <josptrr@gmail.com>
2025-10-31 16:07:31 +0700

    pertemuan4

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,40 +1,27 @@
 <?php
 
-namespace App\Math;
-
 class LuasLingkaran {
 
-    public const phi = 3.14;
-    private int $jari;
-    
-    public function __construct($isiJari = 1) {
-        $this->jari = $isiJari;
-    }
+    const PHI = 3.14;
 
-    // function tampil2($isiJari = 1) {
-    //     // walau protected, bisa diakses di dalam class
-    //     return $this->isiJari;
-    // }
+    public $jari;
 
-
-    public function tampil($nama = 'ban') : void {
-        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
-        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::PHI * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} ini hasilnya adalah: {$rumus}";
     }
 
     public static function testing() {
-        echo "<br/>";
-        echo "ini dari static";
+        echo "</br>";
+        echo "ini testing static";
     }
 
-   public function __destruct() {
-       echo "<br/>";
-       echo "ini dari destructor";
-   }
 }
 
-// $lingkaran = new LuasLingkaran(10);
-// // $lingkaran->jari = 10; // set nilai jari secara manual
-// $lingkaran->tampil('roda'); //panggil method tampil
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 10; //input nilai jari jari
+$lingkaran->tampil(); //tampilkan luas lingkaran
 
-// LuasLingkaran::testing(); //panggil static method
+LuasLingkaran::testing(); //memanggil method static
+//$rumus = Luaslingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+//echo "hasilnya adalah :", $rumus;
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('./luasLingkaran.php');
-
-use App\Math\LuasLingkaran;
-
-$lingkaran = new LuasLingkaran(10);
-// $lingkaran->jari = 10; //input nilai jari-jari
-$lingkaran->tampil('roda'); //tampilkan luas lingkaran
