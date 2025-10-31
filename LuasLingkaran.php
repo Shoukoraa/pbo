3cd2b0b6d009762cdeb48d08ff71975e8901281e
Shoukoraa <josptrr@gmail.com>
2025-10-31 16:05:00 +0700

    pertemuan6

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,13 +1,40 @@
 <?php
 
+namespace App\Math;
+
 class LuasLingkaran {
+
     public const phi = 3.14;
-    public int $jari;
+    private int $jari;
+    
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari;
+    }
+
+    // function tampil2($isiJari = 1) {
+    //     // walau protected, bisa diakses di dalam class
+    //     return $this->isiJari;
+    // }
+
+
+    public function tampil($nama = 'ban') : void {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "ini dari static";
+    }
+
+   public function __destruct() {
+       echo "<br/>";
+       echo "ini dari destructor";
+   }
 }
 
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 7;
+// $lingkaran = new LuasLingkaran(10);
+// // $lingkaran->jari = 10; // set nilai jari secara manual
+// $lingkaran->tampil('roda'); //panggil method tampil
 
-$rumus = LuasLingkaran::phi * $lingkaran->jari * $lingkaran->jari;
-echo 'hasilnya adalah'. $rumus;
-?>
+// LuasLingkaran::testing(); //panggil static method
